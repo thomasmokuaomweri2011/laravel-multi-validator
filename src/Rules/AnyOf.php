db61b2f6b5126b07fa666c656132752dd1e51a16
Thomas Omweri <thomaso@ringier.co.za>
2023-05-29 16:25:43 +0300

    Add custom rules

---
+++ src/Rules/AnyOf.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ThomasOmweri\LaravelOrValidator\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class AnyOf implements ValidationRule
+{
+    /**
+     * Create a new any_of validation rule that returns true if any of the rules is true.
+     *
+     * @return void
+     */
+    public function __construct(protected array $rules)
+    {
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $attribute = str_replace('.', Str::random(), $attribute);
+        $passes = false;
+
+        foreach ($this->rules as $rule) {
+            $data = [
+                $attribute => $value,
+            ];
+            $rules = [
+                $attribute => $rule,
+            ];
+
+            if (Validator::make($data, $rules)->passes()) {
+                $passes = true;
+                break;
+            }
+        }
+
+        if (! $passes) {
+            $fail($this->message());
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        $message = trans('validation.any_of');
+
+        return $message === 'validation.any_of'
+            ? 'None of the specified field rules is true.'
+            : $message;
+    }
+}
---
+++ src/Rules/OrRule.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ThomasOmweri\LaravelOrValidator\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class OrRule implements ValidationRule
+{
+    /**
+     * Create a new or validation rule based on two rules.
+     *
+     * @return void
+     */
+    public function __construct(protected mixed $rule, protected mixed $orRule)
+    {
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  Closure  $fail
+     * @return void
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $attribute = str_replace('.', Str::random(), $attribute);
+
+        $data = [
+            $attribute => $value,
+        ];
+        $rules = [
+            $attribute => $this->rule,
+        ];
+        $orRules = [
+            $attribute => $this->orRule,
+        ];
+
+        if (Validator::make($data, $rules)->fails() && Validator::make($data, $orRules)->fails()) {
+            $fail($this->message());
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return array
+     */
+    public function message()
+    {
+        $message = trans('validation.or_rule');
+
+        return $message === 'validation.or_rule'
+            ? 'None of the specified field rules is true.'
+            : $message;
+    }
+}
