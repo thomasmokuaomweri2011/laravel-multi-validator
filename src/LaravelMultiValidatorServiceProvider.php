072c1e7c5b9af6ed219cfc4e63fb3125d8131cf2
Thomas Omweri <thomaso@ringier.co.za>
2023-06-04 21:48:38 +0300

    Cleanup

--- src/LaravelMultiValidatorServiceProvider.php
+++ src/LaravelMultiValidatorServiceProvider.php
@@ -9,13 +9,6 @@ class LaravelMultiValidatorServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-multi-validator')
-            ->hasConfigFile();
+        $package->name('laravel-multi-validator');
     }
 }
